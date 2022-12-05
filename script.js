4db28b9959712987af06ff3a90a45a98209d7c77
Johan Holm <johan_holm@live.com>
2022-12-05 16:10:58 +0100

    Ändrat hamurgaren responsivitet

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//Kod för att öppna och stänga hamburgermenyn
-$(document).ready(function(){
-    $(".burger-nav").on("click", function(){
-        $(".navbar").toggleClass("open");
-    });
-});
-
+// //Kod för att öppna och stänga hamburgermenyn
+// $(document).ready(function(){
+//     $(".burger-nav").on("click", function(){
+//         $(".navbar").toggleClass("open");
+//     });
+// });
 const form = document.getElementById('form');
 const namn = document.getElementById('namn');
 const ägare = document.getElementById('ägare');
